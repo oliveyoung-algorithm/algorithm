1ecd6340a0c4309f13c1f9bce4cef5865f80c74c
yeyounging <kyy2374@gmail.com>
2026-01-26 14:38:04 +0000

    Time: 55 ms (82.04%), Space: 129.4 MB (24.17%) - LeetHub     source:01b8a4f834bf58178c0f6c89a7d6eaaf4858352a

---
+++ 공예영/5주차/0077-combinations/0077-combinations.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    const result = [];
+
+    function dfs(start, path) {
+        if (path.length === k) {
+            result.push([...path]);
+            return;
+        }
+
+        for (let i = start; i <= n; i++) {
+            path.push(i);
+            dfs(i + 1, path);
+            path.pop();
+        }
+     }
+
+     dfs(1, []);
+
+     return result;
+}       
