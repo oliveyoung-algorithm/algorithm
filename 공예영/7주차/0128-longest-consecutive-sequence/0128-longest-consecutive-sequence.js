3720c4856bb23fc1e049587dbc4cb569e37f6685
yeyounging <kyy2374@gmail.com>
2026-02-10 14:49:59 +0000

    Time: 39 ms (58.68%), Space: 80.8 MB (56.22%) - LeetHub     source:72ae2021a1d91491b4ce60e69abb9becefee4e5f

---
+++ 공예영/7주차/0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    if (nums.length === 0) return 0;
+
+    const numSet = new Set(nums);
+    let maxLen = 0;
+
+    for (const num of numSet) {
+        if (!numSet.has(num - 1)) {
+            let currentNum = num;
+            let currentLen = 1;
+
+            while (numSet.has(currentNum + 1)) {
+                currentNum += 1;
+                currentLen += 1;
+            }
+            maxLen = Math.max(maxLen, currentLen);
+        }
+    }
+
+    return maxLen;
+    
+};
