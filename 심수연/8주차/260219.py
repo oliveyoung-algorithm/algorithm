fb866c56b527d377389205d9e7dd22fa236ce286
letthem <suyeon3662@naver.com>
2026-02-19 14:27:54 +0900

    [BOJ] 2231 분해합 (B2)

---
+++ 심수연/8주차/260219.py
@@ -0,0 +1,14 @@
+# https://www.acmicpc.net/problem/2231
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+for i in range(1, N+1):
+  temp = i + sum((map(int, str(i))))
+  if temp == N: # i가 작은순부터 브루트포스
+    print(i)
+    break
+  if i == N: # i가 끝까지 갔다는 건 계속해서 생성자가 없었다는 뜻 => 0
+    print(0)
