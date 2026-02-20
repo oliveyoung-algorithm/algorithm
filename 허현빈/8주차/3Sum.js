4bd2efed9f9f70e80ab1b6ad5f0ef05c726901b2
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2026-02-21 02:47:15 +0900

    [leet] 3sum 최적화

--- 허현빈/8주차/3Sum.js
+++ 허현빈/8주차/3Sum.js
@@ -6,19 +6,24 @@ var threeSum = function (nums) {
   const arr = nums.sort((a, b) => a - b);
   const ans = [];
   const set = new Set();
+
   const findT = (t, idx) => {
-    let start = 0;
-    let end = nums.length - 1;
+    let start = idx + 1;
+    let end = arr.length - 1;
     if (start === idx) start++;
     if (end === idx) end--;
+
     while (start < end) {
       if (arr[start] + arr[end] === t) {
-        const temp = [arr[idx], arr[start], arr[end]].sort((a, b) => a - b);
+        const temp = [arr[idx], arr[start], arr[end]];
         if (!set.has(temp.join(""))) {
           ans.push(temp);
           set.add(temp.join(""));
         }
         start++;
+        end--;
+        while (start < end && arr[start] === arr[start - 1]) start++;
+        while (start < end && arr[end] === arr[end + 1]) end--;
       } else if (arr[start] + arr[end] < t) {
         start++;
       } else if (arr[start] + arr[end] > t) {
@@ -28,8 +33,7 @@ var threeSum = function (nums) {
       if (end === idx) end--;
     }
   };
-
-  for (let i = 0; i < nums.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i - 1]) continue;
     findT(arr[i] * -1, i);
   }
