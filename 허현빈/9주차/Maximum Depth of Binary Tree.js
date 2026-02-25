6127e05326bd3dab1b2f9b34f32b316d3aea44fd
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2026-02-25 22:38:52 +0900

    [leet] maxDepth

---
+++ 허현빈/9주차/Maximum Depth of Binary Tree.js
@@ -0,0 +1,8 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return 0;
+  return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
