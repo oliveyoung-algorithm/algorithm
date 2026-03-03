7498c5cd08c15f38a15395854331eaceea28ca38
Yonghun Yi <yonghun.yi@cj.net>
2026-03-03 11:54:37 +0900

    [LEET] 55 Jump Game (Medium)

---
+++ 이용훈/10주차/260303.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var canJump = function(nums) {
+    let max = 0;
+
+    for(let i = 0; i < nums.length; i++) {
+        if(i > max) return false;
+
+        max = Math.max(max, i + nums[i]); 
+    }
+
+    return true;
+};
